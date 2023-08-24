c66da78292011c15044e7e0aa561d839eab37001
Cifuentes Bernal <cifam001@mymail.unisa.edu.au>
2023-08-24 16:17:30 +0930

    first commit

---
+++ R/findDCDKM.R
@@ -0,0 +1,13 @@
+#' function to find Dynamic Cancer Drivers Kinetic Models.
+#'
+#' put some description here....
+#'
+#'
+#'
+
+findDCDKM <- function(GE1,GE2 = NULL, pTime1, pTime2
+                      , GT=NULL, PPI = NULL
+                      , maineffect.models = FALSE, perBin=20){
+
+
+}
