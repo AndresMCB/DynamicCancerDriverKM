5d08650a6d075dcf3865d5bfc1d1caf3a473714f
AndresMCB <andresm12@gmail.com>
2023-09-11 16:22:28 +0930

    test

--- demo/Test_DynamicCancerDriverKM(SC).R
+++ demo/Test_DynamicCancerDriverKM(SC).R
@@ -77,3 +77,89 @@ Features <- DCDKM.GeneSelection(Mat1 = mat1, Mat2 = mat2
 # Her2 as covariate for phenopath
 binned <- DCDKM.BinTime(Mat1 = rbind(mat1,mat2), covariate = "ENSG00000141736"
                       , Features = Features, nBins = 30)
+
+
+
+target <- AMCBGeneUtils::changeGeneId(BRCA.40CD)
+
+target <- intersect(target$Ensembl.ID,colnames(binned$Env1))
+results <- vector(mode = "list",length = length(target))
+names(results) <- target
+
+counter <- 0
+library(tictoc)
+for (i in target) {
+  tic()
+  counter <- counter + 1
+  message(paste("Gene ",counter,",", i))
+  predictors <- setdiff(Features,i)
+  predictors <- intersect(predictors,colnames(binned$Env1))
+
+  # put the target variable "y" as the first feature.
+  features <- c(i,predictors)
+  # define the number of top models to be considered (k)
+  k <- length(features)
+  # get the symbolic notation for the models
+  testModels <- DCDKM.GetModels(length(features))
+  invariantScore<-NULL
+  for (j in 1:length(testModels)) {
+    tic()
+    #Score each model (the lower the score the better towards invariance)
+    aux <-  DCDKM.modelScoring(models = testModels[[j]]
+                               , binned = binned, parallel = TRUE
+                               , features = features
+                               , targetIndex = 1, num.folds = 2)
+    invariantScore$score <-c(invariantScore$score
+                             ,aux$modelScores)
+    invariantScore$formulas <-c(invariantScore$formulas
+                                ,aux$formulas)
+    toc()
+  }
+  # retrieve the top k models with better "invariant" score
+  index <- order(invariantScore$score)
+  models <- unlist(testModels, recursive = FALSE)
+  topModels <- models[index[1:k]]
+  #score genes (the more invariant models the gene appears
+  #the higher the score)
+  geneScore <- driverScore(topModels, features)
+  #Inferred genes as genes with score greater than 0
+  InferredDrivers <- geneScore%>%
+    filter(score>0)
+
+  #Assemble the results.
+  results[[i]]$topModels <- topModels
+  results[[i]]$geneScore <- geneScore
+  results[[i]]$InferredDrivers <- InferredDrivers
+  results[[i]]$formulas <- invariantScore$formulas[index[1:k]]
+  results[[i]]$modelScore <- invariantScore$score[index[1:k]]
+  results[[i]]$Summary <- overlapCGC(geneScore = geneScore
+                                     , top = c(seq(50, 250, 50),nrow(InferredDrivers)))
+  toc()
+}
+
+
+####################################################################################
+# save the list of inferred driver from Single cell as excel file
+# (one target per excel sheet)
+temp <-list()
+aux <- AMCBGeneUtils::changeGeneId(names(results),from = "Ensembl.ID")[4]
+
+for (i in names(results)) {
+  temp[[i]] <-
+    cbind(AMCBGeneUtils::changeGeneId(results[[i]]$InferredDrivers[1]
+                                      ,from = "Ensembl.ID", to = c("Ensembl.ID","HGNC.symbol"))[2:3]
+          ,results[[i]]$InferredDrivers[2])
+}
+
+aux <- sapply(aux, function(x){paste("target",x)})
+names(temp) <- aux
+
+
+require(openxlsx)
+write.xlsx(temp, file = "supp_Table9 - inferred drivers from single cell.xlsx")
+names(results) <- target
+
+
+
+
+
